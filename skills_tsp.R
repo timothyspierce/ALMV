aab0ad168bd8283cb229c1bd03856267a42e3b08
austinburcham <austindb@vt.edu>
2021-07-15 13:27:54 -0400

    Minimized number of unmatched soc codes between skills and socfreq

--- skills_tsp.R
+++ skills_tsp.R
@@ -15,7 +15,8 @@ library(readxl)
 #Read in IPUMS data
 ddi <- read_ipums_ddi("usa_00003.xml")
 data <- read_ipums_micro(ddi)
-data %>% filter(STATEFIP %in% state_list) -> app_ipums
+app_ipums <- data %>% filter(STATEFIP %in% state_list) 
+
 
 
 # Filter out unemployed and exchange X's for 9's or 199's
@@ -26,7 +27,10 @@ app_ipums <- app_ipums %>% filter(EMPSTAT == 1)
 app_ipums <- app_ipums %>% 
   mutate(OCCSOC = str_replace_all(OCCSOC, "XXX", "199")) %>% 
   mutate(OCCSOC = str_replace_all(OCCSOC, "XX", "99")) %>% 
-  mutate(OCCSOC = str_replace_all(OCCSOC, "X", "9")) 
+  mutate(OCCSOC = str_replace_all(OCCSOC, "X", "9")) %>% 
+  mutate(OCCSOC = str_replace_all(OCCSOC, "YY", "99")) 
+
+
 
 #Add column for frequency of SOC code (socamt)
 app_ipums <- app_ipums %>%  group_by(OCCSOC) %>% mutate(socamt = n()) 
@@ -36,8 +40,6 @@ socfreq <- app_ipums[,c("OCCSOC", "socamt")]
 colnames(socfreq) <- c("soc", "socfreq")
 socfreq <- distinct(socfreq)
 View(socfreq)
-
-
 
 
 #Read and adjust skills data
@@ -50,6 +52,26 @@ skills <- mutate(skills, soc = gsub("-", "", x = soc))
 
 skills <- mutate(skills, skillname = gsub(" ", "",skillname))
 
+
+# Change soc codes from 2010 to 2019
+new_socs <- read_csv("2010_to_2019_Crosswalk.csv")
+new_socs <- new_socs %>% 
+  mutate(soc = `O*NET-SOC 2010 Code`) %>% 
+  mutate(soc_2019 =`O*NET-SOC 2019 Code` ) %>% 
+  mutate(soc = substr(soc,1,7)) %>% 
+  mutate(soc = gsub("-", "", x = soc)) %>% 
+  mutate(soc_2019 = substr(soc_2019,1,7)) %>% 
+  mutate(soc_2019 = gsub("-", "", x = soc_2019)) %>% 
+  select(soc, soc_2019) %>% distinct()
+
+skills <- inner_join(skills, new_socs, by = "soc") %>% 
+  mutate(soc = soc_2019) %>% 
+  select(-soc_2019)
+skills <- distinct(skills)
+
+
+
+
 # Create skills tibble with individual columns for importance and level
 # Select only the soc's, skillnames and their associated importance and level
 skills_wide <- 
@@ -59,6 +81,21 @@ skills_wide <-
   fill(Level, .direction = "up") %>% 
   select(soc, skillname, Importance, Level) %>% 
   unique()
+
+# Change soc's to match skills info, making socs not in o*net 
+# end in 1 rather than 0. 
+socs <- right_join(skills_wide, socfreq) %>% select(soc, Importance, socfreq)
+## Find soc's only in socfreq
+socs_na <- socs %>% filter(is.na(Importance))
+## change those soc's to end in 1 that end in 0
+altered_na_socs <- socs_na %>% 
+  mutate(soc = str_replace_all(soc, "00$", "99")) %>% 
+  mutate(soc = str_replace_all(soc, "0$", "1")) %>% 
+  select(soc, socfreq)
+## Aggregate all similar socs
+soc_not_na <- socs %>% filter(!is.na(Importance)) %>% select(soc, socfreq)
+## Combine similar and altered socs into one
+altered_socs_freq <- bind_rows(soc_not_na, altered_na_socs)
 
 # Create a standardized table with new "Importance level" column,
 # created through the product of each importance and level ranking 
@@ -80,9 +117,12 @@ skills_indexed <- skills_standardized %>%
 
 # Create a tibble with skills for each soc and their index
 # with associated soc count. 
-skills_indexed_counts <- right_join(skills_indexed, socfreq)
+skills_indexed_counts <- right_join(skills_indexed, altered_socs_freq)
 View(skills_indexed_counts)
 
+# Determine which soc's are null
+null_socs <- skills_indexed_counts %>% filter(is.na(index))
+View(null_socs)
 # ---------------- stop of Austin work -----------------------------------------
 
 #Read in and adjust future jobs data
