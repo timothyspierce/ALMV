408066884996420d7b0480c617918f88560f4aac
austinburcham <austindb@vt.edu>
2021-07-16 10:14:05 -0400

    Isolated 2010 PUMAs to Appalachia, detailed process and upload csv with 2010 Appalachian PUMAs

--- isolating_appalachian_counties.R
+++ isolating_appalachian_counties.R
@@ -14,43 +14,39 @@ library(tigris)
 
 
 # Read in IPUMS data with APPAL and PUMA variables
-ipums_2009_data <- read_ipums_ddi("usa_00005.xml")
+ipums_2009_data <- read_ipums_ddi("usa_00006.xml")
 ipums_2009 <- read_ipums_micro(ipums_2009_data)
 
 # Filter data to only include individuals in Appalachia
 app_data <- ipums_2009 %>% filter(APPAL != 0)
 
 # Create table of unique Appalachian PUMAs and edit to match format of crosswalk
-app_pumas <- as_tibble(unique(app_data$PUMA))
-app_pumas <- app_pumas %>% mutate(PUMA00 = value) %>% 
+app_pumas <- app_data %>% group_by(STATEFIP, PUMA) %>% summarise() %>% ungroup()
+app_pumas <- app_pumas %>% mutate(PUMA00 = PUMA) %>% 
   mutate(PUMA00 = as.character(PUMA00)) %>% 
   mutate(PUMA00 = if_else(
     nchar(PUMA00) == 3, paste0("00", PUMA00), PUMA00)) %>% 
-      mutate(PUMA00 = if_else(nchar(PUMA00) == 4, paste0("0", PUMA00), PUMA00))
-app_pumas <- pull(app_pumas, PUMA00)
-
+  mutate(PUMA00 = if_else(nchar(PUMA00) == 4, paste0("0", PUMA00), PUMA00)) %>% 
+  mutate(State00 = STATEFIP) %>% select(PUMA00, State00) 
+app_pumas <- app_pumas %>%
+  mutate(State00 = as.character(State00), PUMA00 = as.character(PUMA00)) %>% 
+  mutate(State00 = if_else(nchar(State00) == 1, paste0("0", State00), State00))
 
 # Read in crosswalk and condense to only include PUMAs and associated states
 puma_crosswalk <- read_excel("PUMA2000_PUMA2010_crosswalk.xls")
 puma_crosswalk_condensed <- puma_crosswalk %>% 
   select(State00, PUMA00, State10, PUMA10)
 
-# Ensure only Appalachian states are included in crosswalk
-puma_crosswalk_app_states <- puma_crosswalk_condensed %>% 
-  filter(State00 %in% state_list)
+# Create table of all year 2000 app PUMAs and States w their corresponding 2010 values
+puma_crosswalk_app <- 
+  inner_join(app_pumas, puma_crosswalk_condensed, by = c("State00", "PUMA00") )
 
-# Create table of PUMAs that are in appalachia and have only remained the same
-# Through change from 2000 to 2010 PUMAs
-puma_crosswalk_app_pumas <- puma_crosswalk_app_states %>% 
-  filter(PUMA00 %in% app_pumas) %>% 
-  filter(PUMA00 == PUMA10) %>% 
-  distinct()
-
-# Create vector of unique PUMAs that should roughly be in Appalachia
-pumas_2010_app <- unique(puma_crosswalk_app_pumas$PUMA00)
+# Create vector of unique appalachian 2010 PUMAs
+pumas_2010_app <- puma_crosswalk_app %>% group_by(State10, PUMA10) %>% 
+  summarise() %>% distinct()
 
 
-# Obtain list of PUMAs for Appalchian states
+# Obtain list of PUMA sfs for Appalchian states
 options(tigris_use_cache = TRUE)
 puma_geoms_list <- lapply(state_list, function(x) {
   pumas(state = x, cb = T)
@@ -58,9 +54,11 @@ puma_geoms_list <- lapply(state_list, function(x) {
 
 puma_geoms <- rbind_tigris(puma_geoms_list)
 
-# Filter to only include PUMAs that should roughly be in Appalachia
-puma_app_geoms <- puma_geoms %>% filter(PUMACE10 %in% pumas_2010_app)
+# Filter to only include PUMA sfs that should roughly be in Appalachia
+pumas_2010_app <- pumas_2010_app %>%
+  rename(STATEFP10 = State10, PUMACE10 = PUMA10)
+puma_app_geoms <- semi_join(as.data.frame(puma_geoms), pumas_2010_app)
 
 # Plot 
-ggplot(puma_app_geoms) + geom_sf(fill = "coral2") + theme_minimal() +
+ggplot(st_as_sf(puma_app_geoms)) + geom_sf(fill = "coral2") + theme_minimal() +
   coord_sf(datum = NA)
